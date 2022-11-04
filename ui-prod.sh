5b7f90093223fd2a829c3eed6331a28141f5ae8c
Jack Li <jacky984479215@outlook.com>
2022-11-04 15:38:47 -0500

    add frontend deploy script to fast deploy on the vm

---
+++ ui-prod.sh
@@ -0,0 +1,29 @@
+ROOT_DIR=/home/generic/team_1_virtual_TA/wavta
+MOUNTED_APP_DIR=/var/www/ui
+
+# switching to the root directory
+cd $ROOT_DIR
+
+# stop nginx server
+echo "stopping the current nginx server"
+sudo systemctl stop nginx
+
+# install npm dependencies
+echo "installing npm dependencies"
+npm ci
+
+# build webpack
+echo "building webpack"
+npm run build
+
+# remove old app
+echo "removing old app"
+sudo rm -rf $MOUNTED_APP_DIR/*
+
+# add in the new app
+echo "adding new app"
+sudo cp -r $ROOT_DIR/build/* $MOUNTED_APP_DIR
+
+# start nginx web server
+echo "restarting the server"
+sudo systemctl start nginx
