2ac30e4fa2d73eb6f0a96b60fcf4a11d055ec2e2
Olimjon Nematov <macmini@Olimjons-Mac-mini.local>
2022-12-07 13:13:51 -0600

    adding new version of react chatbot kit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-import { Box, Dialog, Divider, Button, Paper } from "@mui/material";
+import { Box } from "@mui/material";
 import ChatBot from "../src/components/chatbot";
 import NavBar from "../src/components/nav-bar";
-import ChatIcon from "@mui/icons-material/Chat";
 
 export default function App() {
   const [dialogOpen, setDialogOpen] = useState(false);
